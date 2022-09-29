54716d1ed583448c5c2d5b136a39cc37d961c6e9
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2022-09-29 13:21:42 -0300

    create formsell

--- src/components/form/Forms.tsx
+++ src/components/form/Forms.tsx
@@ -3,7 +3,7 @@ import { cpf } from 'cpf-cnpj-validator';
 import { useState } from 'react';
 import { Controller, useForm } from "react-hook-form";
 import isEmail from 'validator/lib/isEmail';
-import { IPostClient, IPostProvider } from '../../types';
+import { IPostClient, IPostProvider, IPostRegisterSell } from '../../types';
 import PhoneInput from 'react-phone-number-input'
 import DatePicker from "react-datepicker";
 import router, { useRouter } from 'next/router';
@@ -17,6 +17,7 @@ import { AuthService } from '../../service/AuthService';
 import { sendEmailVerification, sendPasswordResetEmail, signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../../config/firebase'
 import { ServiceProvider } from '../../service/ServiceProvider';
+import { Input } from '@material-ui/core';
 
 
 
@@ -33,7 +34,7 @@ export function FormLogin({ children }) {
         .then((userCredential) => {
             const user = userCredential.user;
             if (user.emailVerified) {
-                router.push("/")
+                router.push("/home")
             }
             else {
                 sendEmailVerification(user)
@@ -125,7 +126,7 @@ export function FormCreateServiceProvider({ children }) {
         }
         const JSONdata = JSON.stringify(data)
      
-        const endpoint = 'http://192.168.0.15:8080/provider/create'
+        const endpoint = 'http://0.0.0.0:8080/provider/create'
         const options = {
             method: 'POST',
             headers: {
@@ -140,7 +141,7 @@ export function FormCreateServiceProvider({ children }) {
                 .then((userCredential) => {
                     const user = userCredential.user;
                     if (user.emailVerified) {
-                        router.push("/")
+                        router.push("/home")
                     }
                     else {
                         sendEmailVerification(auth.currentUser)
@@ -580,4 +581,112 @@ export function FormClient() {
     )
 }
 
+export function FormSell({ children, setPage }) {
+    const router = useRouter();
+    const { control, register, handleSubmit, formState: { errors } } = useForm<IPostRegisterSell>();
+    const [passwordHidden, setPasswordHidden] = useState(true)
+    const [loading, setLoading] = useState(false)
+
+    const myOnSubmit: any = async (submit) => {
+        setLoading(true)
+        try {
+            console.log(submit)
+        } catch (err) {
+            console.log
+        }
+        setPage("confirmSell")
+        setLoading(false)
+    }
+
+    return (
+        <div className='m-4 px-4 flex justify-center items-center pt-20'>
+            <form className='bg-white container lg:mx-auto max-w-sm border-black border-2 rounded-lg shadow-md px-8 pt-6 pb-8 mb-4' onSubmit={handleSubmit(myOnSubmit)}>
+                <div className='mb-4 flex'>
+                    <h1 className='text-left text-4xl font-bold pb-6'>Vender</h1>
+                </div>
+
+                <div className="relative mb-6">
+                    <label>Email do cliente</label>
+                    <input {...register("client_name",
+                        {
+                            required: true,
+                            validate: (value) => {
+                                if (!isEmail(value)) return "Email inválido"
+                            }
+                        })} className='border-2 border-black rounded-lg block w-full p-2.5' type="text"/>
+                    <ErrorMessage errors={errors} name="client_name" />
+                </div>
+                
+                <div className="relative mb-6">
+                    <label>Preço</label>
+                    <input {...register("price",
+                        {
+                            required: true,
+                            validate: (value) => {
+                                if (value < 0) return "Valor inválido"
+                            }
+                        })} className='border-2 border-black rounded-lg block w-full p-2.5' type="text"/>
+                        <ErrorMessage errors={errors} name="price" />
+                </div>
+
+                <div className="relative mb-6">
+                    <label>Sessões</label>
+                    <input {...register("number_of_sessions",
+                        {
+                            required: true,
+                            validate: (value) => {
+                                if (value < 0) return "Valor inválido"
+                            }
+                        })} className='border-2 border-black rounded-lg block w-full p-2.5' type="text"/>
+                        <ErrorMessage errors={errors} name="number_of_sessions" />
+                </div>
+
+                <div className="relative mb-6">
+                    <label>Estudio (Opcional)</label>
+                    <input {...register("studio_name",
+                        {
+                            required: false,
+                            validate: (value) => {
+                                if (value.length > 36 || value.length < 0) return "Valor inválido"
+                            }
+                        })} className='border-2 border-black rounded-lg block w-full p-2.5' type="text"/>
+                        <ErrorMessage errors={errors} name="studio_name" />
+                </div>
+
+                <div className="relative mb-6">
+                    <label>Descrição (Opcional)</label>
+                    <input {...register("description",
+                        {
+                            required: false,
+                            validate: (value) => {
+                                if (value.length > 255) return "Valor inválido"
+                            }
+                        })} className='border-2 border-black rounded-lg block w-full p-2.5' type="text"/>
+                        <ErrorMessage errors={errors} name="description" />
+                </div>
+
+                <div className='relative mb-6'>
+                    {
+                        !loading ? (
+                            <button type="submit" className='bg-black text-white text-sm rounded-lg focus:ring-black block w-full p-2.5'>
+                                Próximo
+                            </button>
+                        ) : (
+                            <button disabled type="button" className="place-content-center bg-black text-white text-sm rounded-lg focus:ring-black block w-full p-2.5 py-2.5 px-5 mr-2 text-sm font-medium text-white bg-white rounded-lg border border-gray-200 inline-flex items-center">
+                                <svg className="inline mr-2 w-6 h-6 text-gray-200 animate-spin dark:text-gray-600 fill-gray-600 dark:fill-gray-300" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                                    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                                </svg>
+                            </button>
+                        )
+                    }
+                </div>
+                {children}
+            </form>
+        </div>
+    )
+}
+function isInteger(value: number): boolean {
+    throw new Error('Function not implemented.');
+}
 
