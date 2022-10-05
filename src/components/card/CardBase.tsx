95c977adfe32e724c81806f26d042c5617b3dbdf
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2022-10-05 14:05:26 -0300

    create component

--- src/components/card/CardBase.tsx
+++ src/components/card/CardBase.tsx
@@ -1,7 +1,7 @@
 export function CardBase({ children }) {
     return (
         <div className='m-4 flex justify-center items-center'>
-            <div className='bg-white container lg:mx-auto max-w-sm border-black border-2 rounded-lg shadow-md px-8 pt-6 pb-8 mb-4'>
+            <div className='bg-white container lg:mx-auto max-w-sm border-black border-2 rounded-lg shadow-md px-4 pt-6 pb-8 mb-4'>
                 {children}
             </div>             
         </div>
@@ -38,9 +38,9 @@ function CardLinkBase({ children }) {
 
 function CardLinkName({ text }) {
     return (
-        <p className="absolute text-xl inset-y-0 left-10 flex items-center px-2">
+        <button className="absolute text-xl inset-y-0 left-10 flex items-center px-2">
             {text}
-        </p>
+        </button>
     )
 }
 
@@ -55,16 +55,19 @@ export function CardCreateClient() {
     )
 }
 
-export function CardSell() {
+export function CardSell({ setPage }) {
     return (
-        <CardLinkBase>
-            <div className="bg-violet-500 rounded-lg block p-1 w-10">
-                <svg className="h-8 w-8 text-white"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"/>
-                </svg>
-            </div>
-            <CardLinkName text="Cadastrar venda"/>
-        </CardLinkBase>
+        <div onClick={setPage}>
+            <CardLinkBase>
+                <div className="bg-violet-500 rounded-lg block p-1 w-10">
+                    <svg className="h-8 w-8 text-white"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"/>
+                    </svg>
+                </div>
+                <CardLinkName text="Cadastrar venda"/>
+            </CardLinkBase>
+        </div>
+        
     )
 }
 
