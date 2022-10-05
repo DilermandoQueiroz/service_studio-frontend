e355703bcbddfe7829432fb9786b6830fa525733
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2022-10-05 14:05:05 -0300

    rename formbutton

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,15 +1,13 @@
 export interface FormInput {
     type: any,
     text: any,
-    placeholder?: any,
     handleOnChange?: any,
-    value?: any
 }
 
-export function FormButton({ type, text, placeholder, handleOnChange, value }: FormInput) {
+export function Button({ type, text, handleOnChange }: FormInput) {
     return (
             <button 
-                className='bg-black text-white rounded-lg focus:ring-black block w-full p-2.5'
+                className='button'
                 type={type}
                 onClick={handleOnChange}
             >
