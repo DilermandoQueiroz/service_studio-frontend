5c19b28f51a8ac2817779b6e49e1f7ce94ba83f5
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2022-10-05 14:05:59 -0300

    update component name

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
 import { withPublic } from '../hook/route'
 import { FormCreateServiceProvider, FormLogin, FormResetPassword} from '../components/form/Forms'
-import { Link } from '../components/Link'
+import { LinkText } from '../components/LinkText'
+import { Terms } from '../components/terms'
 
 const login = () => {
     const [page, setPage] = useState('login');
@@ -9,8 +10,8 @@ const login = () => {
     const login = () => {
         return (
             <FormLogin>
-                <Link text='Não possui conta? Crie uma' handleOnChange={() => setPage('create')}/>
-                <Link text='Esqueceu sua senha?' handleOnChange={() => setPage('reset')}/>
+                <LinkText text='Não possui conta? Crie uma' handleOnChange={() => setPage('create')}/>
+                <LinkText text='Esqueceu sua senha?' handleOnChange={() => setPage('reset')}/>
             </FormLogin>
         )
     }
@@ -18,8 +19,8 @@ const login = () => {
     const create = () => {
         return (
             <FormCreateServiceProvider>
-                <Link text='Você já possui uma conta? Logar' handleOnChange={() => setPage('login')}/>
-                <Link text='Clicando em "Criar conta" você está de acordo com os Termos de Serviço' handleOnChange={() => setPage('terms')}/>
+                <LinkText text='Você já possui uma conta? Logar' handleOnChange={() => setPage('login')}/>
+                <LinkText text='Clicando em "Criar conta" você está de acordo com os Termos de Serviço' handleOnChange={() => setPage('terms')}/>
             </FormCreateServiceProvider>
         )
     }
@@ -27,15 +28,15 @@ const login = () => {
     const reset = () => {
         return (
             <FormResetPassword setPage={setPage}>
-                <Link text="Cancelar" handleOnChange={() => setPage('login')}/>
+                <LinkText text="Cancelar" handleOnChange={() => setPage('login')}/>
             </FormResetPassword>
         )
     }
 
     const confirm = () => {
         return (
-            <div className='m-4 px-4 flex justify-center items-center pt-20'>
-                <div className='bg-white container lg:mx-auto max-w-sm border-black border-2 rounded-lg shadow-md px-8 pt-6 pb-8 mb-4'>
+            <div className='m-4 flex justify-center items-center'>
+                <div className='bg-white container lg:mx-auto max-w-sm border-black border-2 rounded-lg shadow-md px-4 pt-6 pb-8 mb-4'>
                     <div className='mb-4 flex'>
                         <h1 className='text-left text-xl pb-6'>
                             Se uma conta existir com o email informado,
@@ -43,7 +44,7 @@ const login = () => {
                             Se não chegar, verifique sua caixa de spam.
                         </h1>
                     </div>
-                    <Link text="Volte para logar" handleOnChange={() => setPage('login')}/>
+                    <LinkText text="Volte para logar" handleOnChange={() => setPage('login')}/>
                 </div>
             </div>
         )
@@ -51,41 +52,7 @@ const login = () => {
 
     const terms = () => {
         return (
-            <div className='m-4 px-4 flex justify-center items-center pt-20'>
-                <div className='max-w-2xl'>
-                    <div className='mb-4 flex'>
-                        <h1 className='text-center text-4xl font-bold pb-6'>Termos de serviço</h1>
-                    </div>
-                    <div className='mb-4 flex'>
-                        <h1 className='text-left text-xl pb-6'>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                        It has survived not only five centuries, but also the leap into electronic typesetting,
-                        remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets
-                        containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker
-                        including versions of Lorem Ipsum.
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                        It has survived not only five centuries, but also the leap into electronic typesetting,
-                        remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets
-                        containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker
-                        including versions of Lorem Ipsum.
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                        It has survived not only five centuries, but also the leap into electronic typesetting,
-                        remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets
-                        containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker
-                        including versions of Lorem Ipsum.
-                        </h1>
-                    </div>
-                    <button onClick={() => setPage('create')} className='font-bold text-sky-500 w-full p-2.5'>
-                        Criar
-                    </button>
-                </div>
-            </div>
+            <Terms setPage={() => setPage("create")}/>
         )
     }
 
