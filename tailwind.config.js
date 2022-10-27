20238464b8a4246216836734694ad0c9c1ebd40e
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2022-10-27 16:24:32 -0300

    new screens name

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,14 +10,10 @@ module.exports = {
   darkMode: 'media', // or 'class'
   theme: {
     screens: {
-      tablet: '640px',
-      // => @media (min-width: 640px) { ... }
-
-      laptop: '1024px',
-      // => @media (min-width: 1024px) { ... }
-
-      desktop: '1280px',
-      // => @media (min-width: 1280px) { ... }
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
     },
     colors: ({ colors }) => ({
       inherit: colors.inherit,
