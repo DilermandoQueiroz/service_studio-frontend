a6a53c419222bc30a7fbb86eeebe17f52523e7d2
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2022-10-27 16:38:29 -0300

    home component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,13 @@
 import React, { useState, useEffect, useCallback, useRef } from 'react'
+import { Home } from '../components/home'
 import { withPublic } from '../hook/route'
 
 const serviceProviders = () => {
 
   return (
-    <>
-     <div className='mt-10 px-4'>
-      <div className='flex justify-center items-center'>
-        <h1 className='text-5xl font-extrabold'>Página de incio</h1>
-      </div>
-      <div className='flex justify-center items-center'>
-        <h1 className='text-5xl font-extrabold' >Em construção</h1>
-      </div>
-     </div>
-    </>
+    <div>
+      <Home></Home>
+    </div>
   )
 }
 
